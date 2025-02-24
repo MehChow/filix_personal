dfe160ea94dae3a6346c62d16170ef2d63b302e5
Meh Chow <mehchow0604@gmail.com>
2025-02-24 16:05:43 +0800

    Now can automatically detect the LS WiFi connection, and proceed to initialize the instance and connect to the NIR device

---
+++ hooks/use-check-wifi.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+const useCheckWifi = (prefix: string) => {
+  const [detectedLSWifFi, setDetectedLSWiFi] = useState(false);
+
+  useEffect(() => {
+    const loadWifiName = async () => {
+      const state = await NetInfo.fetch();
+      console.log("Current connected wifi: ", state.details?.ssid);
+      if (state.details?.ssid?.startsWith(prefix)) {
+        setDetectedLSWiFi(true);
+        // Clear the interval if LSWiFi is found
+        clearInterval(intervalId);
+      }
+    };
+
+    loadWifiName();
+
+    // Set up an interval to call it every 2 seconds
+    const intervalId = setInterval(loadWifiName, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return detectedLSWifFi;
+};
+
+export default useCheckWifi;
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,5 +1,14 @@
 import { StatusBar } from "react-native";
+import * as Location from "expo-location";
 
 export const getStatusBarHeight = (): number => {
   return StatusBar.currentHeight || 0;
+};
+
+export const askForLocationPermission = async () => {
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    console.log("Permission not granted");
+    return;
+  }
 };
